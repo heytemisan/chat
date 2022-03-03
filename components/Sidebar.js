4aa5f97f82a21986af032c9db761e4bf1f9d1364
Heytemisan <temyicy@gmail.com>
2022-03-03 19:51:13 +0100

    feat: imported mi icons

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,11 +1,20 @@
 import styled from 'styled-components'
+import { Avatar, IconButton } from "@material-ui/core";
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import ChatIcon from '@mui/icons-material/Chat';
 //reusable aspect of our app.
 
 function Sidebar() {
     return (
         <Container>
             <Header>
-
+                <UserAvater />
+                <IconsContainer>
+                    <IconButton>
+                        <ChatIcon />
+                        </IconButton>
+                    <MoreVertIcon/>
+                </IconsContainer>
             </Header>
         </Container>
     )
@@ -15,3 +24,5 @@ export default Sidebar;
 
 const Container = styled.div``;
 const Header = styled.div``;
+const UserAvater = styled(Avatar)``;
+const IconsContainer = styled.div``;
