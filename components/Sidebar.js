51f76fe42d2b62b0d856150232b9f5e8eb26ba06
Heytemisan <temyicy@gmail.com>
2022-03-03 17:39:18 +0100

    added  @material-ui/core@next package

---
+++ components/Sidebar.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+//reusable aspect of our app.
+
+function Sidebar() {
+    return (
+        <Container>
+            <Header>
+
+            </Header>
+        </Container>
+    )
+}
+
+export default Sidebar;
+
+const Container = styled.div``;
+const Header = styled.div``;
