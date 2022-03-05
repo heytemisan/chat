fede70a68ea7d3a23e36551213e7ccfb3545d528
Heytemisan <temyicy@gmail.com>
2022-03-05 09:03:32 +0100

    feat: created a login component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app';
 //check if there's a login using firebase hooks
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, db } from '../firebase';
+import Login from './login'
+
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [user] = useAuthState(auth);
+
+  if(!user) return <Login />
   return <Component {...pageProps} />
 }
 
---
+++ pages/login.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head';
+import React from 'react'
+import styled from 'styled-components';
+
+function login() {
+    return (
+        <Container>
+            <Head>
+                <title>Login</title>
+            </Head>
+        </Container>
+    )
+}
+
+export default login;
+
+const Container = styled.div`
+    
+`
